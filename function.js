fa3c5329d4cc72f64418c38f830b134deae70f2c
Maruthi402 <maruthi.cs402@gmail.com>
2022-11-16 17:58:14 +0530

    My first code

---
+++ function.js
@@ -0,0 +1,73 @@
+// function greet(){
+//     return `Hello ${firstname}`;
+// }
+ 
+// console.log(greet("Maruthi")) 
+
+// const mul = function(a,b){
+//     return a * b
+// }
+
+// console.log(mul(2, 2))
+
+// IIFE Immedietely invokable function expression
+
+// (function (name) {
+//     console.log(`${name}`);
+
+// })("maruthi");
+//--------------------------
+
+// const person = {
+//     getBirthday: function() {
+//         return 1995;
+//     },
+   
+// };
+// console.log(person.getBirthday());
+
+//-------===----------------
+
+// const names = ["Mani", "Rakesh", "Mallikarjjun"];
+
+// // for (i=0; i<names.length; i++){
+// //     console.log(names[i]);
+// // }
+
+// for(v of names){
+//    console.log(names);
+// }
+//-------------------------------
+
+//while loop
+
+let i=0;
+
+// while (i<10){
+//     console.log(i);
+//     i++;
+// }
+// ====================
+
+// do {
+//     i++;
+//     console.log(i);
+
+
+// } while(i < 10);
+// ==========================
+
+const id = 101; //assignment operator
+
+if (id == 100) {
+    console.log("yes the number is 100");
+}else{
+    console.log("sorry the number")
+}
+
+
+
+
+
+
+
