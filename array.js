629a538ef6e46a9c0f82fbc29ef537b52f2a354c
Maruthi402 <maruthi.cs402@gmail.com>
2022-11-16 18:02:16 +0530

    My a

---
+++ array.js
@@ -0,0 +1,19 @@
+const numbers= [25, 32, 5, 46];
+
+const numbers2 = new Array(45, 87, 56, 56);
+
+const fruits = ["Banana", "Orange", "Apples"];
+
+let val;
+
+val = numbers.length;
+
+val = Array.isArray(fruits);
+
+val = numbers[3];
+
+
+console.log(numbers);
+console.log(val);
+// console.log(numbers2);
+// console.log(fruits);
