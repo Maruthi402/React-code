01c481f83c997f735bd328d5081d693787ab1712
Maruthi402 <maruthi.cs402@gmail.com>
2022-11-16 18:04:47 +0530

    My first code

---
+++ switch.js
@@ -0,0 +1,45 @@
+// const color = 'yellow';
+
+// switch(color) {
+//     case 'red':
+//         console.log("color is red");
+//         break;
+//         case 'blue':
+//             console.log('color is blue');
+//             break;
+//             default:
+//                 console.log('Neither blue nor red');
+
+// }
+// ===========================
+
+const dayOfWeek = new Date().getDay();
+let day;
+
+
+switch(dayOfWeek) {
+    case 0:
+        console.log("Sunday");
+        break;
+        case 1:
+            console.log("Monday");
+            break;
+            case 2:
+                console.log("Tusday");
+                break;
+                case 3:
+                    console.log("Wednesday");
+                    break;
+                    case 4:
+                        console.log("Thursday");
+                        break;
+                    case 5:
+                        console.log("Friday");
+                        break;
+                        case 6:
+                            console.log("Saturday");
+                            break;
+                           
+}
+console.log(day);
+
